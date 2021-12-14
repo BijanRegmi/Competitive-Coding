830ad42b6de33074e323a8bef3dc3d62c8d86c5b
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2021-12-15 00:00:10 +0545

    suru gariyo hai la

---
+++ Counting_Valleys.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+int countingValleys(int steps, string path) {
+    bool InsideValley=false;
+    int cnt = 0;
+    int curr_height=0;
+    for (int i=0; i<steps; i++){
+        if (path[i]=='U')
+            curr_height++;
+        else
+            curr_height--;
+        if (!InsideValley && curr_height<0)
+            InsideValley = true;
+        if (InsideValley && curr_height>=0){
+            InsideValley = false;
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string steps_temp;
+    getline(cin, steps_temp);
+
+    int steps = stoi(ltrim(rtrim(steps_temp)));
+
+    string path;
+    getline(cin, path);
+
+    int result = countingValleys(steps, path);
+
+    cout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
