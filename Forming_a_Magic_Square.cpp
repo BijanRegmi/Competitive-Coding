7f14e78b781ec0c7b5d9dcb384240013a04c7d77
Bijan Regmi <vizandabijan@gmail.com>
2021-12-16 21:02:00 +0545

    kati ramailo ahai

---
+++ Forming_a_Magic_Square.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'formingMagicSquare' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY s as parameter.
+ */
+
+int formingMagicSquare(vector<vector<int>> s) {
+    int cost = 0;
+    vector<int> duplicates;
+    set<int> missing{1,2,3,4,5,6,7,8,9};
+    for (auto x: s){
+        for (int y: x){
+            if (missing.count(y))
+                missing.erase(y);
+            else
+                duplicates.push_back(y);
+        }
+    }
+    vector<int> missing_list{missing.begin(), missing.end()};
+
+    for (int i=0, j=duplicates.size(); i<j; i++){
+        cost += abs(duplicates[i] - missing_list[i]);
+    }
+    // return cost;
+
+    for (int x: duplicates)
+        cout << x << endl;
+    cout << endl;
+    for (int x: missing)
+        cout << x << endl;
+
+    return cost;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> s(3);
+
+    for (int i = 0; i < 3; i++) {
+        s[i].resize(3);
+
+        string s_row_temp_temp;
+        getline(cin, s_row_temp_temp);
+
+        vector<string> s_row_temp = split(rtrim(s_row_temp_temp));
+
+        for (int j = 0; j < 3; j++) {
+            int s_row_item = stoi(s_row_temp[j]);
+
+            s[i][j] = s_row_item;
+        }
+    }
+
+    int result = formingMagicSquare(s);
+
+    cout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
