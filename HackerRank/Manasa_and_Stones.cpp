597878807320c42a366e77a3e58debffbb71df77
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2021-12-28 17:08:55 +0545

    hai hai

---
+++ HackerRank/Manasa_and_Stones.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'stones' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER a
+ *  3. INTEGER b
+ */
+
+vector<int> stones(int n, int a, int b){
+    if (n==1) return vector<int>{};
+    else if (n==2){
+        if (a==b) return vector<int> {a};
+        else return vector<int> {a, b};
+    }
+
+    vector<int> child = stones(n-1, a, b);
+    vector<int> left, right;
+    for (int x: child){
+        left.push_back(x+a);
+        right.push_back(x+b);
+    }
+    left.insert(left.end(), right.begin(), right.end());
+    set<int> uni(left.begin(), left.end());
+    left.assign(uni.begin(), uni.end());
+    return left;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string T_temp;
+    getline(cin, T_temp);
+
+    int T = stoi(ltrim(rtrim(T_temp)));
+
+    for (int T_itr = 0; T_itr < T; T_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string a_temp;
+        getline(cin, a_temp);
+
+        int a = stoi(ltrim(rtrim(a_temp)));
+
+        string b_temp;
+        getline(cin, b_temp);
+
+        int b = stoi(ltrim(rtrim(b_temp)));
+
+        vector<int> result = stones(n, a, b);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            cout << result[i];
+
+            if (i != result.size() - 1) {
+                cout << " ";
+            }
+        }
+
+        cout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
